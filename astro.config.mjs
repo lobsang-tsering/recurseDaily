cc4a3cbb272dd9be118e2e9c68e79056c8b4245e
Chris Pennington <chris@codinginpublic.dev>
2024-04-08 06:44:55 -0600

    tutorial code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,32 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://shiki.style/themes
+      // theme: "dracula",
+      // Alternatively, provide multiple themes
+      // https://shiki.style/guide/dual-themes
+      themes: {
+        light: "dracula",
+        dark: "dracula",
+      },
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://shiki.style/languages
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+      // Add custom transformers: https://shiki.style/guide/transformers
+      // Find common transformers: https://shiki.style/packages/transformers
+      transformers: [],
+    },
+  },
 });
